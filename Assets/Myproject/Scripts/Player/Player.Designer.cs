407be371f037395260d69474942662f18d5a5fe0
pigsgod <827024634@qq.com>
2024-04-19 23:55:36 +0800

    no message

---
+++ Assets/Myproject/Scripts/Player/Player.Designer.cs
@@ -0,0 +1,10 @@
+// Generate Id:ec80f875-1172-4a14-83bd-bfa64735d16a
+using UnityEngine;
+
+namespace QFramework.PongPong
+{
+	public partial class Player
+	{
+
+	}
+}
---
+++ Assets/Myproject/Scripts/Player/Player.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using QFramework;
+
+namespace QFramework.PongPong
+{
+	public partial class Player : ViewController
+	{
+		void Start()
+		{
+			// Code Here
+		}
+	}
+}
